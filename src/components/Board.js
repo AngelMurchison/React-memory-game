6221574ea6fa9993b804ba58d2ae661cbb85d69c
Angel Murchison <angelmurchison09@gmail.com>
2017-04-30 20:34:15 -0400

    simple game logic complete, no styling and some bugs

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,22 @@
 import React, { Component } from 'react'
+import Cell from './Cell'
+import image0 from './images/burrito.png'
+import image1 from './images/pizza.jpg'
+import image2 from './images/spaghetti.jpg'
+import image3 from './images/taco.png'
+// where we will store the contents to implant into our cells.
+var contents = [image0, image0, image1, image1, 
+                   image2, image2, image3, image3,
+                   image0, image0, image1, image1,
+                   image2, image2, image3, image3]
+var shuffledContents = []
+
+var content1 = ""
+var content2 = ""
+var index1 = ""
+var index2 = ""
+
+
 // min = 0, max = 16
 var getRandomInt = (min, max) => {
   min = Math.ceil(min);
@@ -6,66 +24,185 @@ var getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-// fill our cells.
-var fillerLogic = (contentArray) => {
-    var contentIndex = getRandomInt(0, 16)    
-    var containerIndex = getRandomInt(0, 16)  
-    if(!usedContainerIndexes.includes(containerIndex) && contentArray[contentIndex] != 'used') {
-        document.getElementById("cell-" + containerIndex).textContent = contentArray[contentIndex]
-        usedContainerIndexes.push(containerIndex)
-        contentArray.splice(contentIndex, 1, 'used')
-        count++
-    }
-}
+// var showImage = (cell) => {
+//     var i = cell.getAttribute("src") - "img-"
+//                     cell.setAttribute("src", shuffledContents[i])
+//                     content2 = content1
+//                     index2 = index1
+//                     content1 = cell.getAttribute("src")
+//                     index1 = i
+//                     console.log("current index:" + index1)
+//                     console.log(content1)
+//                     console.log("previous index:" + index2)
+//                     console.log(content2)
+// }
 
-// how many times do we want our logic to complete.
-var count = 0
-// where we will store the indexes that have been used.
-var usedContainerIndexes = []
-// where we will store the contents to implant into our cells.
-var cellContents = ['A', 'B', 'C', 'D',
-                   'A', 'B', 'C', 'D',
-                   'W', 'X', 'Y', 'Z',
-                   'W', 'X', 'Y', 'Z']
+// var gameLogic = (cell) => {
+    
+//                     if(content1 !== "" && content2 !== ""){
+//                         if(content1 == content2){
+//                             cell.setAttribute("style", "opacity: 1")
+//                             document.getElementById("img-" + index2).setAttribute("style", "opacity: 1")
+
+//                             cell.removeEventListener("click", showImage)
+
+//                             console.log("correct")
+//                             content1 = ""
+//                             content2 = ""
+//                         }
+//                         else if(content1 !== content2){
+//                             cell.setAttribute("style", "opacity: 0")
+//                             document.getElementById("img-" + index2).setAttribute("style", "opacity: 0")
+//                             console.log("incorrect")
+//                             content1 = ""
+//                             content2 = ""
+//                         }
+//                     }
+// }
 
 class Board extends Component {
     constructor (props) {
     super (props);
-
       }
-        componentDidMount() {
+        componentDidMount(){
+            var count = 0
             for (var index = 0; index < 16;) {
                 while (count == index) {
-                    fillerLogic(cellContents)    
+                    var contentIndex = getRandomInt(0, 16)    
+                    var cellIndex = getRandomInt(0, 16)
+
+                    if(!Object.keys(shuffledContents).includes(cellIndex) && contents[contentIndex] != 'used') {
+                    shuffledContents.push(contents[contentIndex])
+                    contents.splice(contentIndex, 1, 'used')
+                    count++
+    }    
                 }
                 index ++
-            }
+            }    
+            console.log(shuffledContents)
+            console.log(Object.keys(shuffledContents))
+            
+
+            Object.keys(shuffledContents).map((v, i, list) => {
+                var cell = document.getElementById("img-" + i)
+                // defining this function in-line with addEventListener() made it impossible to find inside of gameLogic. Binding?
+                var showImage = () => {
+                    cell.style.opacity = "1"
+                    cell.setAttribute("src", shuffledContents[i])
+                    content2 = content1
+                    index2 = index1
+                    content1 = cell.getAttribute("src")
+                    index1 = i
+                }
+                cell.addEventListener("click", showImage)
+                cell.addEventListener("click", function gameLogic(){
+                    if(content1 !== "" && content2 !== ""){
+                        if(content1 == content2){
+                            document.getElementById("img-" + i).style.opacity = "1"
+                            document.getElementById("img-" + index2).style.opacity = "1"
+                            cell.removeEventListener("click", showImage(), true)
+                            cell.id = "disabled"
+                            console.log("correct")
+                            content1 = ""
+                            content2 = ""
+                        }
+                        else if(content1 !== content2){
+                            setTimeout(()=>{
+                            document.getElementById("img-" + i).style.opacity = "0"
+                            document.getElementById("img-" + index2).style.opacity = "0"
+                            console.log("incorrect")
+                            content1 = ""
+                            content2 = ""
+                            }, 500)
+                         }
+                    }
+                })
+
+
+
+                    // cell.addEventListener("click", () => {
+                    // cell.setAttribute("src", shuffledContents[i])
+                    // content2 = content1
+                    // index2 = index1
+                    // content1 = cell.getAttribute("src")
+                    // index1 = i
+                    // console.log("current index:" + index1)
+                    // console.log(content1)
+                    // console.log("previous index:" + index2)
+                    // console.log(content2)
+
+
+                    // if(content1 !== "" && content2 !== ""){
+                    //     if(content1 == content2){
+                    //         cell.setAttribute("style", "opacity: 1")
+                    //         document.getElementById("img-" + index2).setAttribute("style", "opacity: 1")
+
+                    //         cell.removeEventListener("click", this)
+
+                    //         console.log("correct")
+                    //         content1 = ""
+                    //         content2 = ""
+                    //     }
+                    //     else if(content1 !== content2){
+                    //         cell.setAttribute("style", "opacity: 0")
+                    //         document.getElementById("img-" + index2).setAttribute("style", "opacity: 0")
+                    //         console.log("incorrect")
+                    //         content1 = ""
+                    //         content2 = ""
+                    //     }
+                    // }
+
+
+
+                    // if (content1 == ""){
+                    //     content1 = cell.getAttribute("src")
+                    //     index1 = v
+                    //     console.log(content1)
+                    //     console.log(content2)
+                    // }
+                    // if (content2 == ""){
+                    //     content2 = cell.getAttribute("src")
+                    //     index2 = v
+                    //     console.log(content1)
+                    //     console.log(content2)
+                    // }
+                    // while (content1 !== "" && content2 !== ""){
+                    //     if (content1 !== content2) {
+                    //         cell = document.getElementById("img-" + index1)
+                    //         cell.setAttribute("src", "")
+                    //         cell = document.getElementById("img-" + index2)
+                    //         cell.setAttribute("src", "")
+                    //         content1 = ""
+                    //         content2 = ""
+                    //     }
+                    // }
+            })
         }
   render () {
     return <div className="board-container">
         <div id="row-1">
-      <div id="cell-0" className="cell-container"></div>
-      <div id="cell-1" className="cell-container"></div>
-      <div id="cell-2" className="cell-container"></div>
-      <div id="cell-3" className="cell-container"></div>
+            <Cell number="0"  />
+            <Cell number="1"  />
+            <Cell number="2"  />
+            <Cell number="3"  />
       </div>
       <div id="row-2">
-      <div id="cell-4" className="cell-container"></div>
-      <div id="cell-5" className="cell-container"></div>
-      <div id="cell-6" className="cell-container"></div>
-      <div id="cell-7" className="cell-container"></div>
+            <Cell number="4"  />
+            <Cell number="5"  />
+            <Cell number="6"  />
+            <Cell number="7"  />
       </div>
       <div id="row-3">
-      <div id="cell-8" className="cell-container"></div>
-      <div id="cell-9" className="cell-container"></div>
-      <div id="cell-10" className="cell-container"></div>      
-      <div id="cell-11" className="cell-container"></div>
+            <Cell number="8"  />
+            <Cell number="9"  />
+            <Cell number="10" />
+            <Cell number="11" />
       </div>
       <div id="row-4">
-      <div id="cell-12" className="cell-container"></div>
-      <div id="cell-13" className="cell-container"></div>
-      <div id="cell-14" className="cell-container"></div>
-      <div id="cell-15" className="cell-container"></div>
+            <Cell number="12" />
+            <Cell number="13" />
+            <Cell number="14" />
+            <Cell number="15" />
       </div>
     </div>
   }
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react'
 
 class Cell extends Component {
+
   render () {
-    return <div>
-      <h1>Hello, World!</h1>
-    </div>
+    var divId="cell-" + this.props.number  
+    var imgId="img-" + this.props.number
+    return <div id={divId} className="cell-container"><img src={this.props.src}id={imgId} height="100" width="100"/></div>
   }
 }
-
 export default Cell
+
+
+        // componentDidMount() {
+        //             if (click1 = "") {
+        //                 click1 = document.getElementById("img-" + this.props.number)
+        //                 thisClick = click1
+        //             }
+        //             if (click2 = "") {
+        //                 click2 = document.getElementById("img-" + this.props.number)
+        //                 thisClick = click2
+        //             }
+                
+        //         thisClick.addEventListener("click", () => { 
+        //             if( click1 === "" && click2 === "" ) {
+        //                 thisImg = document.getElementById("img-" + this.props.number)
+        //                 thisImg.style.opacity = "1.0"
+        //                 this.setState({src1: thisImg.getAttribute("src").toString(),
+        //                                img1: thisImg})
+        //             }
+        //             else if(this.state.src1 !== "" && this.state.src2 === "") {
+        //                 thisImg = document.getElementById("img-" + this.props.number)
+        //                 thisImg.style.opacity = "1.0"
+        //                 this.setState({src2: thisImg.getAttribute("src").toString(),
+        //                                img2: thisImg})
+
+        //                 if(this.state.src1 === this.state.src2) {
+        //                     this.state.img1.style.opacity = "1.0"
+        //                     this.state.img2.style.opacity = "1.0"
+        //                     this.setState({src1: "",
+        //                                    src2: "", 
+        //                                    img1: "", 
+        //                                    img2: ""})
+        //                 }
+        //                 else if(this.state.src1 != this.state.src2 && this.state.src1 != "" && this.state.src2 != "") {
+        //                     thatImg.style.opacity = "0.2"
+        //                     thisimg.style.opacity = "0.2"
+        //                     this.setState({src1: "", 
+        //                                    src2: "",
+        //                                    img1: "",
+        //                                    img2: ""})
+        //                 }
+        //         }
+        //     })
+        // }
